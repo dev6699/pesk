1439bfb256c4a7b086f5a021b7087215a2df5da1
dev6699 <kweijack.dev@gmail.com>
2026-08-31 17:35:38 +0800

    extract jest config

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  transform: {
+    "^.+\\.tsx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          parser: {
+            syntax: "typescript",
+          },
+        },
+        module: {
+          type: "commonjs",
+        },
+      },
+    ],
+  },
+  testMatch: ["**/tests/**/*.test.ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/shortcuts)\\.js$": "$1",
+    "^\\.\\./shared/shortcuts\\.js$": "<rootDir>/src/renderer/shared/shortcuts.ts",
+    "^\\.\\./\\.\\./shared/shortcuts\\.js$": "<rootDir>/src/renderer/shared/shortcuts.ts",
+    "^\\./default-settings\\.js$": "<rootDir>/src/renderer/shared/default-settings.ts",
+    "^\\.\\./\\.\\./shared/default-settings\\.js$":
+      "<rootDir>/src/renderer/shared/default-settings.ts",
+  },
+};
